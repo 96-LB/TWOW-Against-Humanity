3c19e2f3a2d4f762e6dde6e8ce5b35f802c97f8b
Lucas LaBuff <lucasbeef00@gmail.com>
2020-09-03 03:26:42 +0000

    migrated from jsonbox to local auth storage

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-import gspread, base64, jsonbox, os
+import gspread, base64, os
 
 with open('auth.json', 'w', encoding='utf-8') as f: 
-    f.write(base64.b64decode(jsonbox.JsonBox().read(os.getenv('JSON_BOX'))[0]['auth']).decode('utf-8'))
+    f.write(base64.b64decode(os.getenv('AUTH')).decode('utf-8'))
 client = gspread.service_account('auth.json')
 os.remove('auth.json')
 sheet = client.open_by_key(os.getenv('SHEET'))
